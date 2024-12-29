89c986dbf0cec33acc0d35562a9ada7d7103b39e
aoisora999 <aoisora.ymca@gmail.com>
2024-12-29 09:49:42 -0800

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-api_id = 16418156
-api_hash = "f8431c21d3b515cfd44d7284271d1307"
-bot_token = "6371730093:AAFhEzaf0U8OODzDvYIjRiYx1rDcrmnByUs"
+api_id = 23768009
+api_hash = "213c50b464e39d3bf8e3727b0201b7df"
+bot_token = "5235745411:AAE7GBjR17_9Rrh1UbDd7xuATI513TSNqpQ"
 owner_id = [1962438125]
